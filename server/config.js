e002498126264dbd9b852f62480e43dc291e4b69
jesusdelrioc <jesusdelrioc@gmail.com>
2018-03-07 15:42:06 +0100

    done

--- server/config.js
+++ server/config.js
@@ -2,5 +2,5 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 module.exports = {
-    dbURL: mongoose.connect(process.env.MONGODB_URI),
+    dbURL: process.env.MONGODB_URI,
 }
